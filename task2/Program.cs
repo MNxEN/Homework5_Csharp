96e72c78bb5eb1aceb9abe02030df8c677645624
Mikhail Chekushkin <rbmihey@mail.ru>
2023-06-27 21:33:52 +0500

    Add task2 and task3

---
+++ task2/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 2: 
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedStr = Console.ReadLine();
+    int value;
+    if (int.TryParse(inputedStr, out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine($"Извините, но '{inputedStr}' не является целым числом");
+    Environment.Exit(0);
+    return 0;
+}
+
+int[] GenerateArray(int len, int min, int max)
+{
+    int[] list1 = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < list1.Length; i++)
+    {
+        list1[i] = rnd.Next(min, max + 1);
+    }
+    return list1;
+}
+
+void PrintArray(int[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        System.Console.Write(Array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+void PrintOddPositionSum(int[] Array)
+{
+    System.Console.WriteLine();
+    int sum = 0;
+    for (int i = 1; i < Array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += Array[i];
+        }
+    }
+    System.Console.WriteLine($"Сумма значений на нечетных позициях = {sum}");
+}
+
+int lenArr = Prompt("Введите количество элементов массива");
+int minArr = Prompt("Введите минимальное значение массива");
+int maxArr = Prompt("Введите максимальное значение массива");
+int[] numbers = GenerateArray(lenArr, minArr, maxArr);
+
+PrintArray(numbers);
+
+PrintOddPositionSum(numbers);
+
---
+++ task3/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 3: 
+// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+int PromptInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedStr = Console.ReadLine();
+    int value;
+    if (int.TryParse(inputedStr, out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine($"Извините, но '{inputedStr}' не является целым числом");
+    Environment.Exit(0);
+    return 0;
+}
+double PromptDouble(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedStr = Console.ReadLine();
+    double value;
+    if (double.TryParse(inputedStr, out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine($"Извините, но '{inputedStr}' не является числом");
+    Environment.Exit(0);
+    return 0;
+}
+
+double[] GenerateArray(int len, double min, double max)
+{
+    double[] array = new double[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(rnd.NextDouble() * ((max)-(min))+(min),2); 
+
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+}
+
+(double, double) MinMax(double[]numbers)
+{
+    double min = numbers[0];
+    double max = numbers[0];
+for (int i=0; i < numbers.Length; i++)
+{
+  if (min> numbers[i])
+  {
+    min = numbers[i];
+  }
+  if (max<numbers[i])
+  {
+    max = numbers[i];
+  }
+}
+return(min,max);
+}
+
+int lenArr = PromptInt("Введите количество элементов массива");
+double minArr = PromptDouble("Введите минимальное значение массива");
+double maxArr = PromptDouble("Введите максимальное значение массива");
+
+double[] numbers = GenerateArray(lenArr, minArr, maxArr);
+
+(double min,double max) = MinMax(numbers);
+
+PrintArray(numbers);
+
+double diff = Math.Round(max-min,2);
+
+System.Console.WriteLine($"\n{min} - минимальный элемент массива, {max} - максимальный элемент массива. Разница = {diff}");
